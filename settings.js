12eaa322987fd49cf6ccf0b9115295ee9032ee50
Rizkydisini <115538334+Rizkydisini@users.noreply.github.com>
2023-03-19 19:23:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,9 +28,9 @@ global.antiviewonce = true // anti view once
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 global.autolevelup = true
-global.groupOnly = false //bot hanya bisa di gunakan dalam grup
+global.groupOnly = true //bot hanya bisa di gunakan dalam grup
 
 //language
 //Available in indonesia , english & spanyol 
@@ -41,24 +41,24 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "𝙱𝙾𝚃 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 " //namabot kalian
+global.ownername = "𝚁𝙸𝚉𝙺𝚈𝚂𝚃𝙾𝚁𝙴" //nama kalian
+global.myweb = "https://instabio.cc/rizkystore07" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@RIZKYSTORE7" //bebas asal jan hapus
+global.myweb2 = "https://instabio.cc/rizkystore07" //bebas
+global.email = "rzkygtg664@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['085967019953'] //premium user
+global.owner = ["085967019953",//ganti agar fitur owner bisa di gunakan
+global.ownernomer = "085967019953" // nomor wa kalian
+global.ownernomerr = "+085967019953"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ' + botname //sticker wm ubah
-global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
+global.packname = '© 𝙱𝙾𝚃 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿' + botname //sticker wm ubah
+global.author = '𝚁𝙸𝚉𝙺𝚈𝚂𝚃𝙾𝚁𝙴 ' + ownername //sticker wm ganti nama kalian
 global.sessionName = 'session'
 //type menu for menu
 global.typemenu = 'templateImage' //
@@ -67,7 +67,7 @@ global.typemenu2 = 'templateImage' //
 global.multiplier = 69
 global.limitawal = {
    premium: "Infinity", //unlimited
-   free: 100
+   free: 50
 }
 global.buttonDefault = [{
    urlButton: {
@@ -280,7 +280,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-   sukses: '🤗Done, Oke Desu~',
+   sukses: '🤗Done ya kaka',
    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
